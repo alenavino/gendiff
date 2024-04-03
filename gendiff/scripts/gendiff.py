14dac0f7b5d92f1da34f17e0faa7b403e53d8cb1
alenavino <alenavino1996@gmail.com>
2024-04-03 13:19:35 +0300

    add option to utility

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,6 +10,8 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        help='set format of output')
     args = parser.parse_args()
 
 
