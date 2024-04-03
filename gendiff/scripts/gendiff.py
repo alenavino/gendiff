ec895c82fe32a25e3e4075f0d21c69ea9119087a
alenavino <alenavino1996@gmail.com>
2024-04-03 12:32:58 +0300

    add gendiff and package setup

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
