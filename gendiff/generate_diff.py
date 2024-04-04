79785106ff0abc13a9537ef061dc1ddc0ef6370a
alenavino <alenavino1996@gmail.com>
2024-04-04 18:49:09 +0300

    fix generate-diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,24 +2,24 @@ import json
 
 
 def generate_diff(file1, file2):
-    with (
-        open(file1) as f1,
-        open(file2) as f2,
-    ):
+    with open(file1) as f1:
         file_1 = json.load(f1)
-        file_2 = json.load(f2)
-        result = ['{\n']
-        keys = sorted(set(file_1) | set(file_2))
-        for key in keys:
-            if key in file_1 and key in file_2:
-                if file_1[key] == file_2[key]:
-                    result.append(f'    {key}: {str(file_1[key]).lower()}\n')
-                else:
-                    result.extend([f'  - {key}: {str(file_1[key]).lower()}\n',
-                                  f'  + {key}: {str(file_2[key]).lower()}\n'])
-            elif key in file_1:
-                result.append(f'  - {key}: {str(file_1[key]).lower()}\n')
-            elif key in file_2:
-                result.append(f'  + {key}: {str(file_2[key]).lower()}\n')
-        result.append('}')
-        return ''.join(result)
+        with open(file2) as f2:
+            file_2 = json.load(f2)
+            result = ['{\n']
+            keys = sorted(set(file_1) | set(file_2))
+            for key in keys:
+                if key in file_1 and key in file_2:
+                    if file_1[key] == file_2[key]:
+                        result.append(
+                            f'    {key}: {str(file_1[key]).lower()}\n')
+                    else:
+                        result.extend(
+                            [f'  - {key}: {str(file_1[key]).lower()}\n',
+                             f'  + {key}: {str(file_2[key]).lower()}\n'])
+                elif key in file_1:
+                    result.append(f'  - {key}: {str(file_1[key]).lower()}\n')
+                elif key in file_2:
+                    result.append(f'  + {key}: {str(file_2[key]).lower()}\n')
+            result.append('}')
+            return ''.join(result)
